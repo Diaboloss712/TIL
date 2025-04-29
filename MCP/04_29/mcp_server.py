3af66935f224e3d87ee63304fb4df5af242557c1
Diaboloss712 <diaboloss217+github@gmail.com>
2025-04-29 20:27:17 +0900

    test:call LLM api

--- MCP/04_29/mcp_server.py
+++ MCP/04_29/mcp_server.py
@@ -8,8 +8,6 @@ import os
 import asyncio
 import json
 import traceback
-import pytest
-from unittest.mock import AsyncMock, patch
 
 
 # 설정 값
@@ -24,10 +22,21 @@ load_dotenv()
 REPO_PATH = Path(os.getenv("TIL_ROOT") or Path(__file__).resolve().parents[2])
 GITHUB_USERNAME = os.getenv("GITHUB_USERNAME")
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-MODEL = "mistral"
+MODEL = os.getenv("MODEL")
 OLLAMA_URL = os.getenv("OLLAMA_URL", "http://localhost:11434")
 LOG_FILE = os.getenv("LOG_FILE")
 HEADERS = {
     "Authorization": f"token {GITHUB_TOKEN}",
     "Accept": "application/vnd.github.v3+json"
 }
+
+async def call_LLM_api(prompt:str) -> str:
+    payload = {
+        "model": MODEL,
+        "prompt": prompt,
+        "stream": False
+    }
+    async with httpx.AsyncClient(timeout=60.0) as client:
+        response = await client.post(f'{OLLAMA_URL}/api/generate', json=payload)
+        data = response.json()
+        return data.get("response", "")
---
+++ MCP/04_29/tests/test_mcp_server.py
@@ -0,0 +1,13 @@
+import pytest
+import sys
+import os
+from unittest.mock import AsyncMock, patch
+from mcp_server import(
+    call_LLM_api,
+)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+@pytest.mark.asyncio
+async def test_call_LLM_api():
+    response = await call_LLM_api("다른 대답은 하지 말고 따옴표 안의 글자만 반환해줘. 'New World!'")
+    assert response.strip() == "New World!"
